6c3fd7de510115df2aadc2ee627cfa50a1fbccda
jayvyas <jay@apache.org>
2015-08-09 23:43:04 -0400

    redo suneel's cleanup formatting

--- src/main/java/org/apache/flink/examples/java/bigpetstore/FlinkBPSGenerator.java
+++ src/main/java/org/apache/flink/examples/java/bigpetstore/FlinkBPSGenerator.java
@@ -40,124 +40,124 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 /**
- * 
+ *
  */
 @SuppressWarnings("serial")
 public class FlinkBPSGenerator {
-	 
-	public DataSet<String> generateData(ExecutionEnvironment env){
-		
-		return null;
-	}
-	
 
-	public void writeData(DataSet env){
-		
+    public DataSet<String> generateData(ExecutionEnvironment env) {
 
-	}
-	
-	public static void main(String[] args) throws Exception {
+        return null;
+    }
 
-		final int simulationLength=10;
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		int nStores=100;
-		
-		FlinkBPSGenerator generator = new FlinkBPSGenerator();
-		final InputData id = new DataLoader().loadData();
-		final SeedFactory seedFactory = new SeedFactory(1);
-		StoreGenerator sg = new StoreGenerator(id, seedFactory);
-		final List<Store> stores = Lists.newArrayList();
-		for(int i = 0 ; i < nStores ; i++){
-			stores.add(sg.generate());			
-		}
-		CustomerGenerator cg = new CustomerGenerator(id, stores, seedFactory);
-		final List<Customer> customers = Lists.newArrayList();
-		for(int i = 0 ; i < nStores ; i++){
-			customers.add(cg.generate());			
-		}
-		
-		//now need to put customers into n partitions, and have each partition run a generator.
-		DataStream<Customer> data = env.fromCollection(customers);
-		data.map(
-				new MapFunction<Customer, List<Transaction>>() {
-					public List<Transaction> map(Customer value) throws Exception {
 
-						Collection<ProductCategory> products=id.getProductCategories();
-						PurchasingProfileGenerator profileGen = new PurchasingProfileGenerator(products, seedFactory);
-						PurchasingProfile profile = profileGen.generate();
-						TransactionGenerator transGen = new TransactionGenerator(value, profile, stores, products, seedFactory)
-						List<Transaction> transactions = Lists.newArrayList();
-						Transaction transaction = transGen.generate();
-						transactions.add(transaction);
-						//Create a list of this customer's transactions for the time period
-						while(transaction.getDateTime() < simulationLength) {
-							//TODO implement burn in time like we do in bps-spark
-							transactions.add(transaction);
-							transaction = transGen.generate();
-						}
-						return transactions;
-					}
-				});
-		
+    public void writeData(DataSet env) {
 
-	}
-	
-	// *************************************************************************
-	//     USER FUNCTIONS
-	// *************************************************************************
-	
-	/**
-	 * Implements the string tokenizer that splits sentences into words as a user-defined
-	 * FlatMapFunction. The function takes a line (String) and splits it into 
-	 * multiple pairs in the form of "(word,1)" (Tuple2<String, Integer>).
-	 */
-	public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
 
-		public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
-			// normalize and split the line
-			String[] tokens = value.toLowerCase().split("\\W+");
-			
-			// emit the pairs
-			for (String token : tokens) {
-				if (token.length() > 0) {
-					out.collect(new Tuple2<String, Integer>(token, 1));
-				}
-			}
-		}
-	}
-	
-	// *************************************************************************
-	//     UTIL METHODS
-	// *************************************************************************
-	
-	private static boolean fileOutput = false;
-	private static String textPath;
-	private static String outputPath;
-	
-	private static boolean parseParameters(String[] args) {
-		
-		if(args.length > 0) {
-			// parse input arguments
-			fileOutput = true;
-			if(args.length == 2) {
-				textPath = args[0];
-				outputPath = args[1];
-			} else {
-				System.err.println("Usage: WordCount <text path> <result path>");
-				return false;
-			}
-		} else {
-			System.out.println("Executing WordCount example with built-in default data.");
-			System.out.println("  Provide parameters to read input data from a file.");
-			System.out.println("  Usage: WordCount <text path> <result path>");
-		}
-		return true;
-	}
-	
-	private static DataSet<String> getTextDataSet(ExecutionEnvironment env) {
-		if (fileOutput) {
-			return env.readTextFile(textPath); // read the text file from given input path
-		}
-		return null;
-	}
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        final int simulationLength = 10;
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        int nStores = 100;
+
+        FlinkBPSGenerator generator = new FlinkBPSGenerator();
+        final InputData id = new DataLoader().loadData();
+        final SeedFactory seedFactory = new SeedFactory(1);
+        StoreGenerator sg = new StoreGenerator(id, seedFactory);
+        final List<Store> stores = Lists.newArrayList();
+        for (int i = 0; i < nStores; i++) {
+            stores.add(sg.generate());
+        }
+        CustomerGenerator cg = new CustomerGenerator(id, stores, seedFactory);
+        final List<Customer> customers = Lists.newArrayList();
+        for (int i = 0; i < nStores; i++) {
+            customers.add(cg.generate());
+        }
+
+        //now need to put customers into n partitions, and have each partition run a generator.
+        DataStream<Customer> data = env.fromCollection(customers);
+        data.map(
+                new MapFunction<Customer, List<Transaction>>() {
+                    public List<Transaction> map(Customer value) throws Exception {
+
+                        Collection<ProductCategory> products = id.getProductCategories();
+                        PurchasingProfileGenerator profileGen = new PurchasingProfileGenerator(products, seedFactory);
+                        PurchasingProfile profile = profileGen.generate();
+                        TransactionGenerator transGen = new TransactionGenerator(value, profile, stores, products, seedFactory)
+                        List<Transaction> transactions = Lists.newArrayList();
+                        Transaction transaction = transGen.generate();
+                        transactions.add(transaction);
+                        //Create a list of this customer's transactions for the time period
+                        while (transaction.getDateTime() < simulationLength) {
+                            //TODO implement burn in time like we do in bps-spark
+                            transactions.add(transaction);
+                            transaction = transGen.generate();
+                        }
+                        return transactions;
+                    }
+                });
+
+
+    }
+
+    // *************************************************************************
+    //     USER FUNCTIONS
+    // *************************************************************************
+
+    /**
+     * Implements the string tokenizer that splits sentences into words as a user-defined
+     * FlatMapFunction. The function takes a line (String) and splits it into
+     * multiple pairs in the form of "(word,1)" (Tuple2<String, Integer>).
+     */
+    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+    }
+
+    // *************************************************************************
+    //     UTIL METHODS
+    // *************************************************************************
+
+    private static boolean fileOutput = false;
+    private static String textPath;
+    private static String outputPath;
+
+    private static boolean parseParameters(String[] args) {
+
+        if (args.length > 0) {
+            // parse input arguments
+            fileOutput = true;
+            if (args.length == 2) {
+                textPath = args[0];
+                outputPath = args[1];
+            } else {
+                System.err.println("Usage: WordCount <text path> <result path>");
+                return false;
+            }
+        } else {
+            System.out.println("Executing WordCount example with built-in default data.");
+            System.out.println("  Provide parameters to read input data from a file.");
+            System.out.println("  Usage: WordCount <text path> <result path>");
+        }
+        return true;
+    }
+
+    private static DataSet<String> getTextDataSet(ExecutionEnvironment env) {
+        if (fileOutput) {
+            return env.readTextFile(textPath); // read the text file from given input path
+        }
+        return null;
+    }
 }
